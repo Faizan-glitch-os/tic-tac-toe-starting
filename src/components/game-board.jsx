8026e049e1d05febc5e6badd9b6857eaf4f22476
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-03-28 17:06:33 +0500

    game board can be click to address symbol but still is hardcoded

--- src/components/game-board.jsx
+++ src/components/game-board.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 let initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,29 @@ let initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSymbol(rowIndex, colIndex) {
+    setGameBoard((oldGameBoard) => {
+      const updatedGameBoard = [
+        ...oldGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedGameBoard[rowIndex][colIndex] = "X";
+      return updatedGameBoard;
+    });
+  }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, playerSymbolIndex) => (
               <li key={playerSymbolIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() => handleSymbol(rowIndex, playerSymbolIndex)}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
