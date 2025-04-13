6d4c9408cf369a19e0162a324c1a603ef74c6beb
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-13 18:23:12 +0500

    winner can now be seen but game still continues

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import Player from "./components/player";
 import GameBoard from "./components/game-board";
 import Log from "./components/log";
+import { WINNING_COMBINATIONS } from "./wining-combinations";
+
+let initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurn) {
   let currentPlayer = "X";
@@ -16,6 +23,34 @@ function App() {
   const [gameTurn, setGameTurn] = useState([]);
 
   let activePlayer = deriveActivePlayer(gameTurn);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurn) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleActivePlayer(rowIndex, playerSymbolIndex) {
     setGameTurn((previousTurn) => {
@@ -48,9 +83,10 @@ function App() {
             isActive={activePlayer == "O" ? true : false}
           />
         </ol>
+        {winner && <p>you won!, {winner}</p>}
         <GameBoard
           handleActivePlayerSymbol={handleActivePlayer}
-          turns={gameTurn}
+          board={gameBoard}
         />
       </div>
       <Log turns={gameTurn} />
--- src/components/game-board.jsx
+++ src/components/game-board.jsx
@@ -1,35 +1,7 @@
-let initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ handleActivePlayerSymbol, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //   function handleSymbol(rowIndex, colIndex) {
-  //     setGameBoard((oldGameBoard) => {
-  //       const updatedGameBoard = [
-  //         ...oldGameBoard.map((innerArray) => [...innerArray]),
-  //       ];
-  //       updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //       return updatedGameBoard;
-  //     });
-
-  //     handleActivePlayerSymbol();
-  //   }
+export default function GameBoard({ handleActivePlayerSymbol, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, playerSymbolIndex) => (
